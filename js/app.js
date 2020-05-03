7306ec74dcc5facb1ab06686832900180bf90152
Agustin Sly <agustin_sly@hotmail.com>
2020-05-03 17:48:13 -0300

    Cambios en HTML y CSS
    - Estructurado mejor el html
    - Agregados estilos CSS para fuentes y botones
    - Agregado el modo nocturno beta en JS

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
+function toggleDarkMode() {
+    var element = document.body;    // Obtengo el body del documento para modificar las propiedades.
+    element.classList.toggle("nocturno");   // Toggle agrega o remueve la clase "nocturno" (en CSS), en este caso a cada elemento del body del documento.
+}
 
+function toggleDarkModeExperimental() {
+    for (let cssClass of document.body) {
+        if (cssClass.contains('nocturno')){
+            cssClass.remove('nocturno');
+        }
+        else {
+            cssClass.add('nocturno');
+        }
+    }
+}
