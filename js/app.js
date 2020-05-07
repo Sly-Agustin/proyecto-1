599d1187fc8457b83104b643f21603863e3c23e1
Agustin Sly <agustin_sly@hotmail.com>
2020-05-07 16:49:03 -0300

    Cambios en CSS
    - Agregado Bootstrap
    - Refinado el modo nocturno
    - Agregadas imágenes a las carpetas de imágenes para CSS

--- js/app.js
+++ js/app.js
@@ -1,15 +1,12 @@
 function toggleDarkMode() {
     var element = document.body;    // Obtengo el body del documento para modificar las propiedades.
     element.classList.toggle("nocturno");   // Toggle agrega o remueve la clase "nocturno" (en CSS), en este caso a cada elemento del body del documento.
-}
-
-function toggleDarkModeExperimental() {
-    for (let cssClass of document.body) {
-        if (cssClass.contains('nocturno')){
-            cssClass.remove('nocturno');
-        }
-        else {
-            cssClass.add('nocturno');
-        }
-    }
+    var joystickToggle = document.getElementById("joystick");
+    joystickToggle.classList.toggle("joystickN");
+    /*var headerDiv = element.getElementsByClassName("headerDiv");
+    headerDiv.classList.toggle("headerDiv");*/
+    /*var headerDiv = document.getElementsByClassName("headerDiv")[0];*/
+    /*headerDiv.style.backgroundColor="#FF0000";*/
+    /*var joystickImage = element.getElementsByClassName("joystickN")[0];
+    joystickImage.src="images/joystickInverted.png"*/
 }
