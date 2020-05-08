5ecac6ca10a2729b03253c82468bdf5cffa1c736
Agustin Sly <agustin_sly@hotmail.com>
2020-05-08 18:17:50 -0300

    Cambios en HTML, CSS y JS
    - Cambiados los botones que usaban imagen de background por colores
    - Agregados hover a los botones
    - Mejorado el modo nocturno
    - Agregados estilos CSS
    - Agregada tabla con puntuaciones por operación realizada

--- js/app.js
+++ js/app.js
@@ -2,7 +2,14 @@ function toggleDarkMode() {
     var element = document.body;    // Obtengo el body del documento para modificar las propiedades.
     element.classList.toggle("nocturno");   // Toggle agrega o remueve la clase "nocturno" (en CSS), en este caso a cada elemento del body del documento.
     var joystickToggle = document.getElementById("joystick");
+    console.log(joystickToggle);
     joystickToggle.classList.toggle("joystickN");
+
+    /*No es clean code, habrá alguna manera mejor de hacer esto? Averiguar*/
+    var buttonAux = document.getElementById("buttonDM");
+    buttonAux.classList.toggle("darkModeButton");
+    buttonAux.classList.toggle("lightModeButton");
+
     /*var headerDiv = element.getElementsByClassName("headerDiv");
     headerDiv.classList.toggle("headerDiv");*/
     /*var headerDiv = document.getElementsByClassName("headerDiv")[0];*/
